cfda010296ff0947d4bece6943cf092282c216ba
oleg_baranov <oleg.baranow@gmail.com>
2021-02-25 16:48:53 +0300

    fix tail call

--- TestTask.Core/LinkedListProcessor.cs
+++ TestTask.Core/LinkedListProcessor.cs
@@ -2,17 +2,33 @@ using System.Collections.Generic;
 
 namespace TestTask
 {
-    public class LinkedListProcessor
+    public class LinkedListProcessor<T>
     {
-        public static (Node<T> Head, Node<T> Tail) Reverse<T>(Node<T> node)
+        public Node<T> Head;
+        public Node<T> ReverseOptimized(Node<T> node, Node<T> previous)
         {
-            if (node.Next != null)
+            if (node.Next == null)
             {
-                var (head, reversed) = Reverse(node.Next);
-                reversed.Next = new Node<T>(node.Value);
-                return (head, reversed.Next);
+                Head = node;
+                Head.Next = previous;
+                return node;
             }
-            return (node, node);
+            var next = node.Next;
+            node.Next = previous;
+            return ReverseOptimized(next, node);
         }
+        
+        public Node<T> Reverse(Node<T> node)
+        {
+            if (node.Next == null)
+            {
+                Head = node;
+                return node;
+            }
+            var reversed = Reverse(node.Next);
+            reversed.Next = new Node<T>(node.Value);
+            return Reverse(node.Next);
+        }
+        
     }
 }
--- TestTask.Tests/ReverseTests.cs
+++ TestTask.Tests/ReverseTests.cs
@@ -9,10 +9,50 @@ namespace TestTask.Tests
     [TestFixture]
     public class ReverseTests
     {
-        [Test]
-        public void Reverse_Works([Values("test_one", "test_two")]string sentence)
+        private LinkedListProcessor<char> _processor;
+
+        private string longText = @"Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. 
+Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, 
+very popular during the Renaissance. The first line of Lorem Ipsum, orem ipsum dolor sit amet.., comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
+Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.";
+        [SetUp]
+        public void WarmUp()
         {
-            // Linked list generating
+            _processor = new LinkedListProcessor<char>();
+        }
+        
+        [Test]
+        public void OptimizedReverse_Works()
+        {
+            var sentence = longText;
+            var head = GenerateList(sentence);
+            // Old method doesnt work anymore in this test
+            // _processor.Reverse(head); 
+            _processor.ReverseOptimized(head, null);
+            var array = sentence.ToCharArray();
+            Array.Reverse(array);
+            var reversedString = ReversedString(sentence);
+            Assert.AreEqual( string.Join(string.Empty, array), reversedString);
+        }
+        
+
+        private string ReversedString(string sentence)
+        {
+            var reversedString = new StringBuilder(sentence.Length);
+            var reversedHead = _processor.Head;
+            do
+            {
+                reversedString.Append(reversedHead.Value);
+                reversedHead = reversedHead.Next;
+            } while (reversedHead != null);
+
+            return reversedString.ToString();
+        }
+
+        private static Node<char> GenerateList(string sentence)
+        {
             var index = 0;
             var node = new Node<char>(sentence[0]);
             var head = node;
@@ -24,22 +64,7 @@ namespace TestTask.Tests
                 node = next;
             }
 
-            // Reverse linked list
-            var reversed = LinkedListProcessor.Reverse(head);
-            var array = sentence.ToCharArray();
-            Array.Reverse(array);
-
-            // Compare results
-            var reversedString = new StringBuilder(sentence.Length);
-            var reversedHead = reversed.Head;
-            do
-            {
-                reversedString.Append(reversedHead.Value);
-                reversedHead = reversedHead.Next;
-            } while (reversedHead != null);
-
-            Assert.AreEqual( string.Join(string.Empty, array), reversedString.ToString());
-
+            return head;
         }
     }
 }
